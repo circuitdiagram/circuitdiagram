e4179c92ae54599f765108c9bbc45c98d4224dfa
sfisher <none@none>
2011-02-28 22:20:43 +0000

    --HG-- extra : convert_revision : svn%3Aadd3b90b-c1e4-1f4c-bd44-ac344cdd9602%402

--- CircuitDiagram/CircuitDiagram/Properties/AssemblyInfo.cs
+++ CircuitDiagram/CircuitDiagram/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CircuitDiagramWPF")]
+[assembly: AssemblyTitle("CircuitDiagram")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("CircuitDiagramWPF")]
+[assembly: AssemblyCompany("Circuit Diagram")]
+[assembly: AssemblyProduct("CircuitDiagram")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
