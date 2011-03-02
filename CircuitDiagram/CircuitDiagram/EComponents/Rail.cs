4a6975bbd11078bec82ef7880b05cd2df29ae1f6
sfisher <none@none>
2011-03-02 18:06:59 +0000

    Fixed text alignment on rail component Rearranged order of components in tool bar
    --HG--
    extra : convert_revision : svn%3Aadd3b90b-c1e4-1f4c-bd44-ac344cdd9602%404

--- CircuitDiagram/CircuitDiagram/EComponents/Rail.cs
+++ CircuitDiagram/CircuitDiagram/EComponents/Rail.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Media;
+using System.Globalization;
 
 namespace CircuitDiagram.EComponents
 {
@@ -21,10 +22,11 @@ namespace CircuitDiagram.EComponents
         {
             get
             {
+                FormattedText text = new FormattedText((Voltage > 0 ? "+" : "") + Voltage.ToString() + "V", CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Arial"), 12d, new SolidColorBrush(Colors.Black));
                 if (Horizontal)
-                    return new Rect(new Point(StartLocation.X - 30, StartLocation.Y - 6), new Size(EndLocation.X - StartLocation.X + 30, 12));
+                    return new Rect(new Point(StartLocation.X - (text.Width + 5), StartLocation.Y - 6), new Size(EndLocation.X - StartLocation.X + text.Width + 5, 12));
                 else
-                    return new Rect(new Point(StartLocation.X - 30, StartLocation.Y - 5), new Size(34, EndLocation.Y - StartLocation.Y + 5));
+                    return new Rect(new Point(StartLocation.X - (text.Width + 5), StartLocation.Y - 5), new Size((text.Width + 9), EndLocation.Y - StartLocation.Y + 5));
             }
         }
 
@@ -42,8 +44,9 @@ namespace CircuitDiagram.EComponents
 
         public override void Render(IRenderer dc, Color color)
         {
+            FormattedText text = new FormattedText((Voltage > 0 ? "+" : "") + Voltage.ToString() + "V", CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Arial"), 12d, new SolidColorBrush(Colors.Black));
             dc.DrawLine(color, 2.0f, StartLocation, EndLocation);
-            dc.DrawText((Voltage >0? "+" : "") + Voltage.ToString() + "V", "Arial", 12d, color, Point.Add(StartLocation, new Vector(-30, -5)));
+            dc.DrawText((Voltage >0? "+" : "") + Voltage.ToString() + "V", "Arial", 12d, color, Point.Add(StartLocation, new Vector(-(text.Width + 5), -6)));
         }
 
         public override void LoadData(System.Xml.XmlReader reader)
