6ce48d22663de04532b53c42d6f064e3e61759a4
sfisher <sam@circuit-diagram.org>
2015-08-08 15:39:43 +0100

    Fix build channel selector

--- BuildChannelSelector.ps1
+++ BuildChannelSelector.ps1
@@ -1,12 +1,15 @@
-﻿If ([environment]::GetEnvironmentVariable("APPVEYOR_REPO_TAG")) {
+﻿If ([environment]::GetEnvironmentVariable("APPVEYOR_REPO_TAG") -eq "true") {
     $tag = [environment]::GetEnvironmentVariable("APPVEYOR_REPO_TAG_NAME")
     
-    If ($tag -match "-alpha" -or $tag -match "-beta") {
+    If ($tag -like "v*-alpha*" -or $tag -like "v*-beta*") {
         [environment]::SetEnvironmentVariable("BUILD_CHANNEL_PRE", "TRUE", "User")
         "Build channel: Prerelease"
-    } Else {
+    } ElseIf ($tag -like "v*") {
         [environment]::SetEnvironmentVariable("BUILD_CHANNEL_STABLE", "TRUE", "User")
         "Build channel: Stable"
+    } Else {
+        [environment]::SetEnvironmentVariable("BUILD_CHANNEL_NIGHTLY", "TRUE", "User")
+        "Build channel: Nightly"
     }
 } Else {
     [environment]::SetEnvironmentVariable("BUILD_CHANNEL_NIGHTLY", "TRUE", "User")
