9baf2e8f925a256c60618bcae51a3ed4aa2c6d17
sfisher <sam@circuit-diagram.org>
2015-08-09 15:09:20 +0100

    Bulid setup

---
+++ Setup/CreateSetup.ps1
@@ -0,0 +1,12 @@
+﻿$productName = "Circuit Diagram (Nightly)"
+$productVersion = [environment]::GetEnvironmentVariable("APPVEYOR_BUILD_VERSION");
+If ([environment]::GetEnvironmentVariable("BUILD_CHANNEL_STABLE") -eq "true") {
+    $productName = "Circuit Diagram"
+} ElseIf ([environment]::GetEnvironmentVariable("BUILD_CHANNEL_PRE") -eq "true") {
+    $productName = "Circuit Diagram (Prerelease)"
+}
+
+& "C:\Program Files (x86)\WiX Toolset v3.10\bin\candle.exe" Product.wxs -ext WixUIExtension `
+    "-dProductName=$productName" `
+    "-dProductVersion=$productVersion"
+& "C:\Program Files (x86)\WiX Toolset v3.10\bin\light.exe" -out Output\Setup.msi Product.wixobj -ext WixUIExtension
